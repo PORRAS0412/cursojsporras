a2ccd929806fbc334502ff4646062a72cafbff1c
root <root@andres.jaimetorres.net>
2023-07-10 18:01:03 -0500

    arrais

--- Curso JS Moderno/08-Arrays/js/03-app.js
+++ Curso JS Moderno/08-Arrays/js/03-app.js
@@ -9,4 +9,5 @@ console.log(meses.length);
 
 for(let i=0;i < meses.length;i++){
     console.log(meses[i])
+   
 }
--- Curso JS Moderno/08-Arrays/js/pruebas.js
+++ Curso JS Moderno/08-Arrays/js/pruebas.js
@@ -10,5 +10,10 @@ const carrito = [
 
 for (let i = 0; i < carrito.length; i++){
 console.table(carrito[i].nombre)
-console.log()
+console.log(`${carrito[i].nombre} precio : ${carrito[i].precio}`)
 }
+
+
+carrito.forEach(function(hola){
+    console.log(`${producto.nombre} precio : ${producto.precio}`)
+});
